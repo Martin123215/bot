b415ee9af17381eaec7f4fb4914471c4bf0f0a5c
Martin123215 <reyescristopher018@gmail.com>
2026-06-24 12:02:16 -0400

    Actualizar app.js

--- app.js
+++ app.js
@@ -8,17 +8,27 @@ import {
 }
 from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
- apiKey: "TU_API_KEY",
- authDomain: "TU_PROYECTO.firebaseapp.com",
- projectId: "TU_PROYECTO",
-
+  apiKey: "AIzaSyDkqZfjuT4yBPAKKU5f1eXlqEW7-zeg1GE",
+  authDomain: "bots-9de8a.firebaseapp.com",
+  projectId: "bots-9de8a",
+  storageBucket: "bots-9de8a.firebasestorage.app",
+  messagingSenderId: "830406173368",
+  appId: "1:830406173368:web:cdaa7c75365dbcdeaa8a4d",
+  measurementId: "G-BD322NC94J"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
 window.guardar = async () => {
 
