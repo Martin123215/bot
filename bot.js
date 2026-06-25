7b205bd39440a34fd4c4034bf68eff888e70abd4
Martin123215 <reyescristopher018@gmail.com>
2026-06-24 21:58:59 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,10 @@ const client = new Client({
 
 client.once('ready', async () => {
   console.log(`Bot conectado como ${client.user.tag}`);
+
+  const canal = await client.channels.fetch('1519365482656829561');
+
+  await canal.send('🚚 Bot funcionando correctamente');
 });
 
 client.login(process.env.DISCORD_TOKEN);
